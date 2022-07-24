853030b7bd04675a758196f63a06f2822b43e08b
Guilherme Vieira Silva <guivieirasilva02@gmail.com>
2022-07-24 15:13:45 -0300

    Criando a carta com React

---
+++ src/components/CardElement.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+export default function CardElement(props){
+  return(
+    <div id={props.card.id} className="card" >
+      <div className="card_front">
+        <img 
+          src={`assets/imagens/${props.card.icon}.png`} 
+          className="icon" 
+          alt={props.card.icon}
+        />
+      </div>
+
+      <div className="card_back">
+        {"</>"}
+      </div>
+    </div>
+  )
+}
