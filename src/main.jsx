e4dccc4493943b914bfd1e82e6eb3da44d30eae2
Guilherme Vieira Silva <guivieirasilva02@gmail.com>
2022-07-24 14:24:42 -0300

    Iniciando o Projeto com React

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import { MemoryGame} from "./MemoryGame"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <MemoryGame />
   </React.StrictMode>
 )
