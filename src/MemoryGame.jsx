483a67565cdc15ec4649b10f85a288bf41f986f4
Guilherme Vieira Silva <guivieirasilva02@gmail.com>
2022-07-24 15:25:03 -0300

    Criando o tabuleiro

--- src/MemoryGame.jsx
+++ src/MemoryGame.jsx
@@ -1,9 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+
+import GameBoard from "./components/GameBoard";
 import GameOver from "./components/GameOver";
+
+import game from './game/game'
 
 export function MemoryGame(){
 
   const [gameOver, setGameOver] = useState(true)
+  const [cards, setCards] = useState([])
+
+  useEffect(() => {
+    setCards(game.createCardFromTechs())
+  },[])
 
   function restart(){
     setGameOver(false)
@@ -11,6 +20,7 @@ export function MemoryGame(){
 
   return(
     <div>
+      <GameBoard cards={cards} />
       <GameOver handleRestart={restart} show={gameOver}/>
     </div>
   )
--- src/components/CardElement.jsx
+++ src/components/CardElement.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function CardElement(props){
   return(
-    <div id={props.card.id} className="card" >
+    <div id={props.card.id} className="card flip" >
       <div className="card_front">
         <img 
           src={`assets/imagens/${props.card.icon}.png`} 
